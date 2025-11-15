536f33e568b9281d255562ef6bf749ecf98d5a78
MinhSoora <minhsoora@gmail.com>
2025-11-15 11:45:49 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,7 @@ import NotFound from "../pages/404";
 import Projects from "../pages/Projects";
 import ProjectDetails from "../pages/Projects/projectsInfo";
 import Donate from "../pages/donate";
-import Contact from "../pages/contact";
 import Calendar from "../pages/Calendar";
-import NapThe from "../pages/napthe";
 
 const publicRoutes = [
   { path: config.routes.home, component: About },
@@ -21,9 +19,7 @@ const publicRoutes = [
   { path: config.routes.projectDetails, component: ProjectDetails },
   { path: config.routes.notfound, component: NotFound },
   { path: config.routes.donate, component: Donate },
-  { path: config.routes.contact, component: Contact },
   { path: config.routes.calendar, component: Calendar },
-  { path: config.routes.napthe, component: NapThe },
 ];
 const privateRoutes = [];
 
